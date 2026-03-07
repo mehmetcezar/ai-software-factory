86612d7782b8a13d5b0a72fa588a232e9cafb038
LOG <84563831+mehmetcezar@users.noreply.github.com>
2026-03-07 18:29:18 +0200

    fix: revert accidental password replacements during rebrand

--- config.php
+++ config.php
@@ -1,8 +1,4 @@
 <?php
-$config=array(
-'DB_HOST'=>'localhost',
-'DB_USERNAME'=>'u973221251_whitelotus',
-'DB_PASSWORD'=>'Noveltech2022!!',
-'DB_DATABASE'=>'u973221251_whitelotus'
+$config = array(    'DB_HOST' => 'localhost',    'DB_USERNAME' => 'u973221251_whitelotus',    'DB_PASSWORD' => 'Whitelotus2022!!',    'DB_DATABASE' => 'u973221251_whitelotus'
 );
 ?>
